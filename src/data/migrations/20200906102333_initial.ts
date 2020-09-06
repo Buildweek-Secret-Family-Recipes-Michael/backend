f251239d67695345550bd6f58594a42e534ec771
ClydeFrog04 <egan.randal@gmail.com>
2020-09-06 14:58:33 -0600

    chore(migrations): added date column
    Added a registered-at column to the users table

--- src/data/migrations/20200906102333_initial.ts
+++ src/data/migrations/20200906102333_initial.ts
@@ -8,6 +8,7 @@ export async function up(knex: Knex): Promise<void> {
         tableBuilder.uuid("id").unique().notNullable().primary();
         tableBuilder.text("username").unique().notNullable();
         tableBuilder.text("password").notNullable();
+        tableBuilder.timestamp("registered-at", {precision:6}).notNullable();
     });
 
     await knex.schema.createTable("recipes", tableBuilder => {
