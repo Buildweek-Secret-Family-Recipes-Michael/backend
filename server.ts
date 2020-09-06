17d9040e7dcf27b027692cad9a9365bb28a94a12
ClydeFrog04 <egan.randal@gmail.com>
2020-09-06 14:33:54 -0600

    chore(index.ts): abstract out server
    Took server logic out of index.ts, created server.ts file

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
 import express from "express";
+import {usersRouter} from "./src/routes/users/users-router";
+
+export const server = express();
+
+server.use(express.json());
+server.use("/api/users", usersRouter);
+
+server.get("/", async (req, res) => {
+    res.send("Welcome!");
+});
