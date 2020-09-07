553177962ecb5d2d07cef03b078ebcc2e7892606
ClydeFrog04 <egan.randal@gmail.com>
2020-09-06 23:42:11 -0600

    feat(recipes-router.ts): get recipes
    Get recipes route create and restricted to logged in users

--- src/routes/recipes-router.ts
+++ src/routes/recipes-router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as recipesModel from "../models/recipes-model";
 import {validateRecipeInfo} from "../middleware/recipesMiddleware";
+import {restrict} from "../middleware/usersMiddleware";
 
 export const recipesRouter = express.Router();
 
@@ -19,6 +20,16 @@ recipesRouter.post("/", validateRecipeInfo, async (req, res) => {
 
 
 //read
+recipesRouter.get("/", restrict, async (req, res) => {
+    //this is a restricted route and a user will need to be signed in. Check get recipes method in recipes-model
+    try {
+        const recipes = await recipesModel.getRecipes();
+        res.status(200).json(recipes);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error getting recipes"});
+    }
+});
 
 
 //update
