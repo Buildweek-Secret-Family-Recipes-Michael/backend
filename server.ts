b2c8e66814a5599a5df46b929f5b179e0351d8cf
ClydeFrog04 <egan.randal@gmail.com>
2020-09-18 19:40:40 -0600

    fix(cors): fix cors

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {usersRouter} from "./src/routes/users-router";
 import {recipesRouter} from "./src/routes/recipes-router";
 import {ingredientsRouter} from "./src/routes/ingredients-router";
+
 import cors from "cors";
 
 export const server = express();
